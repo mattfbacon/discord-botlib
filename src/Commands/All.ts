5c136dc1b5ed29e1ba4049d37971bf4576b27365
Matt Fellenz <mattf53190@gmail.com>
2021-05-19 19:47:06 -0700

    Accidentally commited Test command

--- src/Commands/All.ts
+++ src/Commands/All.ts
@@ -1,11 +1,9 @@
 import type { CommandHandler, } from '../Command';
 
 import Help from './Help';
-import Test from './Test';
 
 export { help, } from './Help';
 
 export default new Map<string, CommandHandler>([
 	[ "help", Help, ],
-	[ "test", Test, ],
 ]);
